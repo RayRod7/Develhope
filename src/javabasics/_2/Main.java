b3fb31dd4cc3714cdbeb7739574e7d032809a919
Raimondo <raimondo.rodriguez94@gmail.com>
2023-03-07 16:19:48 +0100

    test-first-commit

---
+++ src/javabasics/_2/Main.java
@@ -0,0 +1,8 @@
+package javabasics._2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world 1!");
+        System.out.print("Hello world!");
+    }
+}
---
+++ src/javabasics/_3/Main.java
@@ -0,0 +1,4 @@
+package javabasics._3;
+
+public class Main {
+}
