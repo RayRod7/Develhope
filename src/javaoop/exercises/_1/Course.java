1137489382c9cef2f78e66d6932de45beaff815f
Raimondo <raimondo.rodriguez94@gmail.com>
2023-03-20 16:57:45 +0100

    exercise javaoop 1 done

---
+++ src/javaoop/exercises/_1/Course.java
@@ -0,0 +1,9 @@
+package javaoop.exercises._1;
+
+public class Course {
+    String courseName = "";
+    int maxStudents = 0;
+    double qualityRatingOutOf10 = 0.0;
+    int DurationOfTheCourseInMonth= 0;
+    String teacher = "";
+}
---
+++ src/javaoop/exercises/_1/Student.java
@@ -0,0 +1,6 @@
+package javaoop.exercises._1;
+
+public class Student {
+    String name = " ";
+    Integer age = 0;
+}
